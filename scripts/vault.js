db39487252570d30d494a465bd84605f1e56e62e
PanoramicPanda <uriah.h.brown@gmail.com>
2024-04-07 17:12:45 -0400

    Move functions out to different files to divide responsibilities

--- scripts/vault.js
+++ scripts/vault.js
@@ -26,29 +26,6 @@ function negativeMod(die) {
     counter.textContent = currentValue - 1;
 }
 
-async function loadSavedRolls() {
-    try {
-        const savedData = await TS.localStorage.campaign.getBlob();
-        const savedRolls = JSON.parse(savedData || '[]');
-        savedRolls.forEach(roll => {
-            addSavedRoll(roll.name, roll.type, roll.counts);
-        });
-    } catch (e) {
-        console.error('Failed to load saved rolls:', e);
-    }
-}
-
-function saveCurrentRolls() {
-    const savedRollsElements = document.querySelectorAll('.saved-roll-entry');
-    const savedRolls = Array.from(savedRollsElements).map(roll => {
-        return {
-            name: roll.querySelector('.roll-entry-label').textContent,
-            type: roll.dataset.rollType,
-            counts: JSON.parse(roll.dataset.diceCounts)
-        };
-    });
-}
-
 function sortSavedRolls() {
     const sortOption = document.getElementById('sort-options').value;
     const savedRollsContainer = document.querySelector('.saved-rolls-container');
@@ -385,142 +362,12 @@ async function displayResult(resultGroup, rollId) {
     TS.dice.sendDiceResult([resultGroup], rollId).catch((response) => console.error("error in sending dice result", response));
 }
 
-function toggleSettingsDisplay() {
-    const settingsContainer = document.getElementById('settings-menu');
-    const settingsButton = document.getElementById('settings-button');
-    settingsContainer.classList.toggle('hidden');
-    settingsButton.classList.toggle('active-menu');
-}
-
-function defaultSettings(settingName){
-    const settings = {
-        autoLoadRolls: false,
-        autoSaveRolls: false,
-        autoResetEdit: false,
-        critBehavior: 'double-die-count'
-    }
-    return settings[settingName];
-}
-
-function saveGlobalSettings(){
-    const settings = {
-        autoLoadRolls: document.getElementById('auto-load').checked,
-        autoSaveRolls: document.getElementById('auto-save').checked,
-        autoResetEdit: document.getElementById('auto-reset').checked,
-        critBehavior: document.getElementById('crit-behavior').value
-    }
-    TS.localStorage.global.setBlob(JSON.stringify(settings)).then(() => {
-        console.log('Settings saved successfully.');
-    }).catch(error => {
-        console.error('Failed to save settings:', error);
-    });
-    updateAutoButtons();
-}
-
-function loadGlobalSettings(){
-    TS.localStorage.global.getBlob().then(settingsJson => {
-        const settings = JSON.parse(settingsJson || '{}');
-        document.getElementById('auto-load').checked = settings.autoLoadRolls || defaultSettings('autoLoadRolls');
-        document.getElementById('auto-save').checked = settings.autoSaveRolls || defaultSettings('autoSaveRolls');
-        document.getElementById('auto-reset').checked = settings.autoSaveRolls || defaultSettings('autoResetEdit');
-        document.getElementById('crit-behavior').value = settings.critBehavior || defaultSettings('critBehavior');
-        performAutoLoads();
-    }).catch(error => {
-        console.error('Failed to load settings:', error);
-    });
-}
-
-function fetchSetting(settingName){
-    const setting = document.getElementById(settingName)
-    if (setting === null){
-        console.error('Setting not found:', settingName);
-        return;
-    }
-
-    if (setting.type === 'checkbox'){
-        return setting.checked;
-    }
-
-    if (setting.type === 'select-one'){
-        return setting.value;
-    }
-}
-
-function saveRollsToLocalStorage() {
-    let rollsData = [];
-    document.querySelectorAll('.saved-roll-entry').forEach(entry => {
-        let rollData = {
-            name: entry.querySelector('.roll-entry-label').textContent.trim(),
-            type: entry.dataset.rollType,
-            counts: JSON.parse(entry.dataset.diceCounts)
-        };
-        rollsData.push(rollData);
-    });
-
-    let rollsJson = JSON.stringify(rollsData);
-    console.log('Saving rolls data:', rollsJson); 
-    TS.localStorage.campaign.setBlob(rollsJson).then(() => {
-        console.log('Rolls data saved successfully.');
-        disableButtonById('save-rolls-button');
-        disableButtonById('load-rolls-button')
-    }).catch(error => {
-        console.error('Failed to save rolls data:', error);
-    });
-}
-
-function loadRollsFromLocalStorage() {
-    TS.localStorage.campaign.getBlob().then(rollsJson => {
-        console.log('Loading rolls data:', rollsJson);
-        let rollsData = JSON.parse(rollsJson || '[]');
-        rollsData.forEach(rollData => {
-            addSavedRoll(rollData.name, rollData.type, rollData.counts);
-        });
-        disableButtonById('load-rolls-button');
-        if (!fetchSetting('auto-save')){
-            disableButtonById('save-rolls-button', false);
-        }
-    }).catch(error => {
-        console.error('Failed to load rolls data:', error);
-    });
-}
-
 async function onStateChangeEvent(msg){
     if (msg.kind === 'hasInitialized'){
         loadGlobalSettings();
     }
 }
 
-function performAutoLoads(){
-    if (fetchSetting('auto-load')) {
-        console.log('Auto-loading rolls from local storage.');
-        loadRollsFromLocalStorage();
-
-    }
-
-    if (fetchSetting('auto-save')) {
-
-    }
-
-    updateAutoButtons();
-}
-
-function updateAutoButtons(){
-    if (fetchSetting('auto-load')) {
-        document.getElementById('load-rolls-button').innerText = 'Auto-Loading';
-        disableButtonById('load-rolls-button');
-    }else{
-        document.getElementById('load-rolls-button').innerText = 'Load Rolls';
-        disableButtonById('load-rolls-button', false);
-    }
-
-    if (fetchSetting('auto-save')) {
-        document.getElementById('save-rolls-button').innerText = 'Auto-Saving';
-        disableButtonById('save-rolls-button');
-    } else {
-        document.getElementById('save-rolls-button').innerText = 'Save Rolls';
-        disableButtonById('save-rolls-button', false);
-    }
-}
 
 function disableButtonById(id, disable = true){
     document.getElementById(id).disabled = disable;
