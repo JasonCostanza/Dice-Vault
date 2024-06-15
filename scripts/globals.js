b5d7cda1499130d0883c6b550099dc9addd2bda3
JasonCostanza <30665880+JasonCostanza@users.noreply.github.com>
2024-06-15 09:56:49 -0700

    globals.js

---
+++ scripts/globals.js
@@ -0,0 +1,8 @@
+//let trackedIds = {};
+//let isGM = false;
+//let me;
+//let gmRolls = {};
+let savedInVault = []; // Array of arrays, contains diceGroupsData[] arrays as elements which is everything saved to JSON ./localstorage/ inside one array
+const diceGroupsData= [0];
+const diceTypes = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20'];
+const diceGroups = [];
--- scripts/vault.js
+++ scripts/vault.js
@@ -1,18 +1,5 @@
-//let trackedIds = {};
-//let isGM = false;
-//let me;
-//let gmRolls = {};
-let savedInVault = []; // Array of arrays, contains diceGroupsData[] arrays as elements which is everything saved to JSON ./localstorage/ inside one array
-const diceGroupsData= [0];
-const diceTypes = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20'];
-const diceGroups = [];
-
 document.addEventListener('DOMContentLoaded', sortSavedRolls);
-document.addEventListener('DOMContentLoaded', initializeRowZero);
 
-function initializeRowZero() {
-    
-}
 
 function increment(type) {
     const counterId = type + '-counter-value';
