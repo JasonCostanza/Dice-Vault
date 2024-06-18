1faace05b0d5ff0a0a4e8aed86a781dc17bc13fd
JasonCostanza <30665880+JasonCostanza@users.noreply.github.com>
2024-06-18 16:48:58 -0700

    wip

--- scripts/rolls.js
+++ scripts/rolls.js
@@ -42,17 +42,11 @@ function roll(rollNameParam, rollTypeParam) {
     }
 
     // Construct the dice roll string from the dice groups
-    let TSDiceRollString = constructDiceRollString(rollName, diceGroupsData);
-
-    // Validate the constructed dice roll string
-    if (!TS.dice.isValidRollString(TSDiceRollString)) {
-        console.error('Invalid dice roll string:', TSDiceRollString);
-        return;
-    }
+    let diceRollObjects = constructDiceRollString(rollName);
 
     try {
         // Create the roll object with name and roll string
-        let rollObject = { name: rollName, roll: TSDiceRollString };
+        // let rollObject = { name: rollName, roll: diceRollObjects};
         let rollCount;
 
         // Determine the roll count based on the selected type
@@ -69,7 +63,7 @@ function roll(rollNameParam, rollTypeParam) {
         }
 
         // Create the tray configuration for the specified number of rolls
-        let trayConfiguration = Array(rollCount).fill(rollObject);
+        let trayConfiguration = diceRollObjects;
 
         // Put dice in tray and handle the response
         TS.dice.putDiceInTray(trayConfiguration, true).then(diceSetResponse => {
@@ -84,6 +78,93 @@ function roll(rollNameParam, rollTypeParam) {
         console.error('Error creating roll descriptors:', error);
     }
 }
+
+// function roll(rollNameParam, rollTypeParam) {
+//     let selectedType = rollTypeParam || 'normal'; // Set to normal if no type is provided
+
+//     let updatedDiceGroupsData = [];
+
+//     diceGroupsData.forEach((group, index) => {
+//         let groupId = index;
+//         let groupDiceCounts = {};
+//         diceTypes.forEach(type => {
+//             const counter = document.getElementById(`${groupId}-${type}-counter-value`);
+//             if (counter) {
+//                 groupDiceCounts[type] = counter.textContent;
+//             } else {
+//                 console.error(`Could not find counter for ${groupId}-${type}`);
+//             }
+//         });
+
+//         const modCounter = document.getElementById(`${groupId}-mod-counter-value`);
+//         if (modCounter) {
+//             groupDiceCounts['mod'] = modCounter.value;
+//         } else {
+//             console.error(`Could not find mod counter for ${groupId}`);
+//         }
+
+//         updatedDiceGroupsData.push(groupDiceCounts);
+//     });
+
+//     diceGroupsData = updatedDiceGroupsData;
+
+//     let critBehavior = fetchSetting('crit-behavior');
+
+//     let rollName = buildRollName(rollNameParam, selectedType, critBehavior);
+
+//     // Adjust for critical hit dice types
+//     if (selectedType === 'crit-dice') {
+//         if (critBehavior === 'double-die-count') {
+//             diceCounts = doubleDieCountsForGroups(group)
+//         }
+//         selectedType = 'normal';  // Reset to normal type after handling critical dice
+//     } else {
+//         critBehavior = 'none';
+//     }
+
+//     // Construct the dice roll string from the dice groups
+//     let TSDiceRollString = constructDiceRollString(rollName, diceGroupsData);
+
+//     // Validate the constructed dice roll string
+//     if (!TS.dice.isValidRollString(TSDiceRollString)) {
+//         console.error('Invalid dice roll string:', TSDiceRollString);
+//         return;
+//     }
+
+//     try {
+//         // Create the roll object with name and roll string
+//         let rollObject = { name: rollName, roll: TSDiceRollString };
+//         let rollCount;
+
+//         // Determine the roll count based on the selected type
+//         switch (selectedType) {
+//             case 'advantage':
+//             case 'disadvantage': // if selectedType is 'advantage' or 'disadvantage, fall through to rollCount = 2
+//                 rollCount = 2;
+//                 break;
+//             case 'best-of-three':
+//                 rollCount = 3;
+//                 break;
+//             default:
+//                 rollCount = 1;
+//         }
+
+//         // Create the tray configuration for the specified number of rolls
+//         let trayConfiguration = Array(rollCount).fill(rollObject);
+
+//         // Put dice in tray and handle the response
+//         TS.dice.putDiceInTray(trayConfiguration, true).then(diceSetResponse => {
+//             // Track the rolled dice IDs with their type and critical behavior
+//             trackedIds[diceSetResponse] = {
+//                 type: selectedType,
+//                 critBehavior: critBehavior
+//             };
+//         });
+//     } catch (error) {
+//         // Log any errors encountered during roll descriptor creation
+//         console.error('Error creating roll descriptors:', error);
+//     }
+// }
 
 function buildRollName(rollNameParam, rollTypeParam, critBehaviorParam) {
     let rollName = rollNameParam || document.getElementById('roll-name').value || 'Unnamed Roll';
@@ -124,8 +205,8 @@ function formatRollTypeName(rollType) {
 }
 
 function constructDiceRollString(rollName) {
-    // Create an empty array to store the formatted dice group strings
-    let formattedDiceGroups = [];
+    // Create an empty array to store the dice roll objects
+    let diceRollObjects = [];
 
     // Iterate over each dice group in the diceGroupsData array
     for (const groupDiceCounts of diceGroupsData) {
@@ -151,15 +232,53 @@ function constructDiceRollString(rollName) {
 
         // Join the formatted dice and modifier strings for the current group into a single string
         let groupRollString = formattedDiceGroup.join('');
-        // Push the group roll string to the formattedDiceGroups array
-        formattedDiceGroups.push(groupRollString);
+        // Create a roll object with the roll name and group roll string
+        let rollObject = { name: rollName, roll: groupRollString };
+        // Push the roll object to the diceRollObjects array
+        diceRollObjects.push(rollObject);
     }
 
-    // Construct the final Talespire dice roll string by encoding the roll name and joining the formatted dice groups with '/'
-    let TSDiceRollString = formattedDiceGroups.join('/');
-    // Return the constructed Talespire dice roll string
-    return TSDiceRollString;
+    // Return the array of dice roll objects
+    return diceRollObjects;
 }
+
+// function constructDiceRollString(rollName) {
+//     // Create an empty array to store the formatted dice group strings
+//     let formattedDiceGroups = [];
+
+//     // Iterate over each dice group in the diceGroupsData array
+//     for (const groupDiceCounts of diceGroupsData) {
+//         // Create an empty array to store the formatted dice and modifier for the current group
+//         let formattedDiceGroup = [];
+
+//         // Iterate over each die type and its count in the current group
+//         for (const [die, count] of Object.entries(groupDiceCounts)) {
+//             // If the die type is not 'mod' and the count is greater than 0
+//             if (die !== 'mod' && count > 0) {
+//                 // Push the formatted die string (e.g., "1d4") to the formattedDiceGroup array
+//                 formattedDiceGroup.push(`${count}${die}`);
+//             }
+//         }
+
+//         // If the modifier for the current group is not 0
+//         if (groupDiceCounts.mod !== 0) {
+//             // Determine the modifier string based on whether it's positive or negative
+//             let modPart = groupDiceCounts.mod >= 0 ? `+${groupDiceCounts.mod}` : `${groupDiceCounts.mod}`;
+//             // Push the modifier string to the formattedDiceGroup array
+//             formattedDiceGroup.push(modPart);
+//         }
+
+//         // Join the formatted dice and modifier strings for the current group into a single string
+//         let groupRollString = formattedDiceGroup.join('');
+//         // Push the group roll string to the formattedDiceGroups array
+//         formattedDiceGroups.push(groupRollString);
+//     }
+
+//     // Construct the final Talespire dice roll string by encoding the roll name and joining the formatted dice groups with '/'
+//     let TSDiceRollString = formattedDiceGroups.join('/');
+//     // Return the constructed Talespire dice roll string
+//     return TSDiceRollString;
+// }
 
 async function handleRollResult(rollEvent) {
     if (trackedIds[rollEvent.payload.rollId] == undefined) {
